04a0e35c5c802de2ee95d2f2d8890340a0c9113a
Patrick Zhang <patrickjyzhang@gmail.com>
2023-06-08 03:52:14 -0700

    fix bug where baseurl left out

--- client/src/components/ViewInfo/ViewInfo.js
+++ client/src/components/ViewInfo/ViewInfo.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 //import {getResponseData} from './getResponseData';
-import Axios from 'axios';
-import MovieInfo from './Infos/MovieInfo';
-import TVInfo from './Infos/TVInfo';
-import PersonInfo from './Infos/PersonInfo';
-import ErrorMessage from './../AlertMessages/ErrorMessage';
-import LoadingMessage from './../AlertMessages/LoadingMessage';
+import Axios from "axios";
+import MovieInfo from "./Infos/MovieInfo";
+import TVInfo from "./Infos/TVInfo";
+import PersonInfo from "./Infos/PersonInfo";
+import ErrorMessage from "./../AlertMessages/ErrorMessage";
+import LoadingMessage from "./../AlertMessages/LoadingMessage";
 
+const baseURL = process.env.REACT_APP_API_BASE_URL;
 
 //Gets the primary details for the item we want to view more info about.
 
@@ -18,119 +19,131 @@ import LoadingMessage from './../AlertMessages/LoadingMessage';
     viewInfo:
     closeInfo:
     origSearch: 
-*/ 
+*/
 
 class ViewInfo extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      data: null,
 
-        this.state = {
-            data: null,
+      loading: true,
+      error: false,
+    };
+  }
 
-            loading: true,
-            error: false,
+  //Can put this code in constructor instead to reduce performance issues
+  componentDidMount() {
+    this.url = `${baseURL}/api/${this.props.type}/${this.props.id}`;
+    this.setState({ loading: true });
+
+    Axios.get(this.url).then(
+      (response) => {
+        console.log("Successfully got a response for: " + this.url);
+        this.setState({ data: response.data, loading: false, error: false });
+      },
+      (error) => {
+        console.log(error);
+        this.setState({ data: null, loading: false, error: true });
+      }
+    );
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.id !== this.props.id || prevProps.type !== this.props.type) {
+      if (prevState.data != null) {
+        this.setState({ data: null }); //Clear out previous data, if any.
+      }
+
+      this.setState({ loading: true });
+
+      this.url = `${baseURL}/api/${this.props.type}/${this.props.id}`;
+      Axios.get(this.url).then(
+        (response) => {
+          console.log("Successfully got a response for: " + this.url);
+          this.setState({ data: response.data, loading: false, error: false });
+        },
+        (error) => {
+          console.log(error);
+          this.setState({ data: null, loading: false, error: true });
         }
+      );
+    }
+  }
 
-        
+  render() {
+    //If we did not successfully get a response, will need to display an error message.
+    if (this.state.error) {
+      console.log("Loading ViewInfo.js");
+      const errorMsg =
+        "Could not retrieve data! Please reload the page and try again.";
+      return (
+        <div className="view-info">
+          <ErrorMessage message={errorMsg} />
+        </div>
+      );
+    } else if (this.state.loading) {
+      console.log("Loading ViewInfo.js");
+      const loadingMsg = "Loading...";
+      return (
+        <div className="view-info">
+          <LoadingMessage message={loadingMsg} />
+        </div>
+      );
     }
 
-    //Can put this code in constructor instead to reduce performance issues
-    componentDidMount() {
-        this.url = `/api/${this.props.type}/${this.props.id}`;
-        this.setState({loading: true});
-
-        Axios.get(this.url).then(
-            (response) => {
-                console.log("Successfully got a response for: " + this.url);
-                this.setState({data: response.data, loading: false, error: false});
-            }, 
-            (error) => {
-                console.log(error);
-                this.setState({data: null, loading: false, error: true});
-            }
+    var infoComponent;
+    switch (this.props.type) {
+      case "movie":
+        infoComponent = (
+          <MovieInfo
+            movieDetails={this.state.data}
+            viewInfo={this.props.viewInfo}
+            closeInfo={this.props.closeInfo}
+          />
         );
+        break;
+      case "tv":
+        infoComponent = (
+          <TVInfo
+            tvDetails={this.state.data}
+            viewInfo={this.props.viewInfo}
+            closeInfo={this.props.closeInfo}
+          />
+        );
+        break;
+      case "person":
+        infoComponent = (
+          <PersonInfo
+            personDetails={this.state.data}
+            viewInfo={this.props.viewInfo}
+            closeInfo={this.props.closeInfo}
+          />
+        );
+        break;
+      default:
+        //If type does not match any of these cases, will need to display an error message.
+        console.log("props.type not recognized in ViewInfo.js");
+        infoComponent = null; //TODO: display an error message instead!
+        break;
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        if(prevProps.id !== this.props.id || prevProps.type !== this.props.type) {
-
-            if(prevState.data != null) {
-                this.setState({data: null}); //Clear out previous data, if any.
-            }
-
-            this.setState({loading: true});
-
-            this.url = `/api/${this.props.type}/${this.props.id}`;
-            Axios.get(this.url).then(
-                (response) => {
-                    console.log("Successfully got a response for: " + this.url);
-                    this.setState({data: response.data, loading: false, error: false});
-                }, 
-                (error) => {
-                    console.log(error);
-                    this.setState({data: null, loading: false, error: true});
-                }
-            );
-        }
-    }
-
-    render() {
-        //If we did not successfully get a response, will need to display an error message.
-        if(this.state.error) {
-            
-            console.log("Loading ViewInfo.js");
-            const errorMsg = "Could not retrieve data! Please reload the page and try again.";
-            return (
-                <div className="view-info">
-                    <ErrorMessage message={errorMsg} />
-                </div>
-            )
-
-        } else if(this.state.loading) {
-            
-            console.log("Loading ViewInfo.js");
-            const loadingMsg = "Loading...";
-            return (
-                <div className="view-info">
-                    <LoadingMessage message={loadingMsg} />
-                </div>
-            )
-        }
-        
-        var infoComponent; 
-        switch(this.props.type) {
-            case('movie'): 
-                infoComponent = <MovieInfo movieDetails={this.state.data} 
-                    viewInfo={this.props.viewInfo} closeInfo={this.props.closeInfo}/>;
-                break;
-            case('tv'):
-                infoComponent = <TVInfo tvDetails={this.state.data}
-                    viewInfo={this.props.viewInfo} closeInfo={this.props.closeInfo}/>;
-                break;
-            case('person'):
-                infoComponent = <PersonInfo personDetails={this.state.data}
-                    viewInfo={this.props.viewInfo} closeInfo={this.props.closeInfo}/>;
-                break;
-            default: 
-                //If type does not match any of these cases, will need to display an error message.
-                console.log("props.type not recognized in ViewInfo.js");
-                infoComponent = null; //TODO: display an error message instead!
-                break;
-        }
-
-        return (<div className="view-info">
-                    <button className="back_button" onClick={() => this.props.closeInfo()}>
-                        <i className="fas fa-angle-left fa-3x"></i>
-                        <span>
-                            Back to {this.props.prevName ? `'${this.props.prevName}'` 
-                            : `results for '${this.props.origSearch}'`}
-                        </span>
-                    </button>
-                    {infoComponent}
-                </div>);
-    }
-     
+    return (
+      <div className="view-info">
+        <button className="back_button" onClick={() => this.props.closeInfo()}>
+          <i className="fas fa-angle-left fa-3x"></i>
+          <span>
+            Back to{" "}
+            {this.props.prevName
+              ? `'${this.props.prevName}'`
+              : `results for '${this.props.origSearch}'`}
+          </span>
+        </button>
+        {infoComponent}
+      </div>
+    );
+  }
 }
 
 export default ViewInfo;
